7d2d4d8fa2780ccabbdee7c5fca7b079b8861b4f
= <luke@lukezsmith.com>
2022-08-23 21:32:57 +0100

    robots.txt

--- lukezsmith/urls.py
+++ lukezsmith/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': {'blogpost': GenericSitemap(blog_info_dict, priority=0.6)}},
      name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt',TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     re_path('.*', TemplateView.as_view(template_name="index.html")),
 ]
 
